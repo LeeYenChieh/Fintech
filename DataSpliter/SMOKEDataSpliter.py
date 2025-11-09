9b92d93a94b4b6330363501194852f87b68c10cb
mjlee <yenchieh0826@gmail.com>
2025-11-09 15:16:28 +0800

    add smoke dataspliter

--- DataSpliter/SMOKEDataSpliter.py
+++ DataSpliter/SMOKEDataSpliter.py
@@ -8,15 +8,15 @@ class SMOKEDataSpliter(DataSpliter):
 
     def splitData(self, X, y):
         smote = SMOTE(random_state=42, sampling_strategy='auto', k_neighbors=5)
+        X_no_acct = X.drop(columns=['acct'])
+        X_resampled, y_resampled = smote.fit_resample(X_no_acct, y)
 
         X_train_final, X_val, y_train_final, y_val = train_test_split(
-            X, y, 
+            X_resampled, y_resampled, 
             test_size=0.2,      # 驗證集比例 20%
-            stratify=y,   # 根據 y 保持正負樣本比例
+            stratify=y_resampled,   # 根據 y 保持正負樣本比例
             random_state=42
         )
 
-        X_resampled, y_resampled = smote.fit_resample(X_train_final, y_train_final)
-
         print("(Finish) Split Data.")
-        return X_resampled, X_val, y_resampled, y_val
+        return X_train_final, X_val, y_train_final, y_val
