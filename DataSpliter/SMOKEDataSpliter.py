a53e64499c5981f52f7a03be6878bc7777a46fb5
mjlee <yenchieh0826@gmail.com>
2025-11-09 20:22:13 +0800

    update smoke spliter: No Smoke in validation

--- DataSpliter/SMOKEDataSpliter.py
+++ DataSpliter/SMOKEDataSpliter.py
@@ -7,17 +7,19 @@ class SMOKEDataSpliter(DataSpliter):
         super().__init__()
 
     def splitData(self, X, y):
-        smote = SMOTE(random_state=42, sampling_strategy='auto', k_neighbors=5)
-        X_no_acct = X.drop(columns=['acct'])
-        X_resampled, y_resampled = smote.fit_resample(X_no_acct, y)
-        X_resampled["acct"] = "None"
 
-        X_train_final, X_val, y_train_final, y_val = train_test_split(
-            X_resampled, y_resampled, 
+        X_train, X_val, y_train, y_val = train_test_split(
+            X, y, 
             test_size=0.2,      # 驗證集比例 20%
             stratify=y_resampled,   # 根據 y 保持正負樣本比例
             random_state=42
         )
 
+        smote = SMOTE(random_state=42, sampling_strategy='auto', k_neighbors=5)
+
+        X_no_acct = X_train.drop(columns=['acct'])
+        X_resampled, y_resampled = smote.fit_resample(X_no_acct, y_train)
+        X_resampled["acct"] = "None"
+
         print("(Finish) Split Data.")
-        return X_train_final, X_val, y_train_final, y_val
+        return X_resampled, X_val, y_resampled, y_val
