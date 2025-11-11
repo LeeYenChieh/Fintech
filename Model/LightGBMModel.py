4857a80a016a495040d4e3c2f743f7d2f3b2ec1d
MJLee <mjleecsie0826@gamail.com>
2025-11-11 18:28:12 +0800

    requirment

--- Model/LightGBMModel.py
+++ Model/LightGBMModel.py
@@ -48,6 +48,7 @@ class LightGBM(Model):
             'boosting_type': 'gbdt',
             'device': 'gpu',               # 啟用 GPU 訓練
             'gpu_device_id': -1,
+            'num_thread': 70,
             'verbosity': 2,
             'seed': 42,
 
